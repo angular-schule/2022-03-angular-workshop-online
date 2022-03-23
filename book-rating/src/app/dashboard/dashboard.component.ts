5912739e171018d1a205eb3d6a4d820bdcbfe5d7
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-23 14:52:38 +0100

    Buchliste mit ngFor

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  books: Book[] = [];
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        price: 36.9,
+        rating: 5
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        price: 32.9,
+        rating: 3
+      },
+    ];
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+/*
+class BookC {
+  rating: number = 5;
+  constructor(public isbn: string, public title: string) {}
+
+  rateUp() {
+    this.rating++;
+  }
+}
+
+const myBook = new BookC('123', 'Angular');
+myBook.rateUp();
+
+////////
+
+interface BookI {
+  isbn: string;
+  title: string;
+  rating: number;
+}
+
+
+const myBook2: BookI = {
+  isbn: '1234',
+  title: 'Angular',
+  rating: 5
+};
+
+
+const myDTO = {
+  "isbn": "1234",
+  "title": "Angular",
+  "rating": 5
+}
+
+
+
+const b1 = new BookC(myDTO.isbn, myDTO.title)
+
+
+const b2 = {
+  ...myDTO,
+  titlex: myDTO.title
+}
+
+
+function rateUp(book: BookI): BookI {
+  book.rating++;
+  return book;
+}
+
+*/
+
+
