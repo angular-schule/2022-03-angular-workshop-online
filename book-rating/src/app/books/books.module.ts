b195d081ce4ed58ef633a1528c6e0e51b4d71f0f
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-24 10:29:51 +0100

    ng g m books --routing

---
+++ book-rating/src/app/books/books.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BooksRoutingModule } from './books-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BooksRoutingModule
+  ]
+})
+export class BooksModule { }
