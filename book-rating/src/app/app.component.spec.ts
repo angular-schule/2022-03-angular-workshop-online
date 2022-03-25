4de51b2ae7da6fa02f89af146c531501c0b20924
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-25 12:16:14 +0100

    Tests AppComponent

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
@@ -20,16 +22,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'book-rating'`, () => {
+  it(`should have as title 'Book Rating'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title).toEqual('Book Rating');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('book-rating app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating');
   });
 });
