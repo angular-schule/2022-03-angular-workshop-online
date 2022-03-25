74e6e6bb30439db9b79c7920c07fc8ee41d1f8fc
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-25 12:16:08 +0100

    Tests Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,42 @@
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let book: Book;
 
   beforeEach(async () => {
+    // Testbuch
+    book = {
+      isbn: '',
+      title: '',
+      description: '',
+      price: 44,
+      rating: 5
+    };
+
+    const ratingMock: BookRatingService = {
+      rateUp: b => b,
+      rateDown: b => b
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      schemas: [NO_ERRORS_SCHEMA], // Shallow Component Test
+      providers: [
+        // BRS ersetzen: Immer wenn BookRatingService angefordert wird,
+        // wird stattdessen ratingMock ausgeliefert
+        {
+          provide: BookRatingService,
+          useValue: ratingMock
+        }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +49,22 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service for doRateUp()', () => {
+    // Arrange
+    // Service anfordern. Tatsächlich kommt hier der ratingMock raus, weil der Provider das so festlegt
+    const service = TestBed.inject(BookRatingService);
+
+    // service überwachen. Originale Methode rateUp soll aber trotzdem verwendet werden!
+    spyOn(service, 'rateUp').and.callThrough();
+
+    // Act
+    component.doRateUp(book);
+
+
+    // Assert
+    expect(service.rateUp).toHaveBeenCalledTimes(1);
+    expect(service.rateUp).toHaveBeenCalledWith(book);
   });
 });
