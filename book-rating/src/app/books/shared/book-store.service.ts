09f87e9a71acaae2ee8a88192c0e71798e2c3b88
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-28 15:05:33 +0200

    Environments

--- book-rating/src/app/books/shared/book-store.service.ts
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Book } from './book';
 
 @Injectable({
@@ -9,6 +10,7 @@ import { Book } from './book';
 export class BookStoreService {
 
   private apiUrl = 'https://api.angular.schule';
+  private apiUrl2 = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
