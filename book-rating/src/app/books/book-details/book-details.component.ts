583ec1e718f23dc6cba7444dcca53509721f638e
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-30 12:28:34 +0200

    Detailseite: Refactoring mit switchMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -17,15 +18,14 @@ export class BookDetailsComponent implements OnInit {
     // const isbn = this.route.snapshot.paramMap.get('isbn'); // books/:isbn
 
     // Asynchroner Weg / PUSH
-    // TODO: Verschachtelte Subscriptions vermeiden
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn')!; // Non-Null Assertion
-      console.log(isbn);
 
-      this.bs.getSingle(isbn).subscribe(book => {
-        this.book = book;
-      });
+    this.route.paramMap.pipe(
+      map(params => params.get('isbn')!),
+      switchMap(isbn => this.bs.getSingle(isbn))
+    ).subscribe(book => {
+      this.book = book;
     });
+
 
 
     /* AUFGABE
