584b5cf6e2756109461127d70681f5728452adb7
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-30 13:27:17 +0200

    NgRx: Einrichtung
    ng add @ngrx/store
    ng add @ngrx/store-devtools
    ng add @ngrx/effects
    
    ng add @ngrx/schematics --default-collection

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 // import { BooksModule } from './books/books.module';
 
 @NgModule({
@@ -16,7 +20,10 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     AppRoutingModule,
     // BooksModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
