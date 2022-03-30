8947357beab541b8ff25eb283dd7d3c39774687d
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-30 13:53:35 +0200

    Typeahead-Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { debounceTime, filter, map, Observable, switchMap } from 'rxjs';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-search',
@@ -11,12 +13,16 @@ export class BookSearchComponent implements OnInit {
 
   searchControl = new FormControl('');
 
-  constructor() {
+  result$: Observable<Book[]>;
+
+  constructor(private bs: BookStoreService) {
     const input$: Observable<string> = this.searchControl.valueChanges;
 
-    input$.subscribe(e => {
-      console.log(e);
-    })
+    this.result$ = input$.pipe(
+      debounceTime(300),
+      filter(term => term.length >= 3),
+      switchMap(term => this.bs.search(term))
+    );
   }
 
   ngOnInit(): void {
